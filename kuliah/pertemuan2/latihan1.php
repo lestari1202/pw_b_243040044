e790c4297ba661bf9586573ac5f57616b33dd3bf
lestari1202 <lestarivebrianti12@gmail.com>
2025-02-17 19:06:28 +0700

    selesai mengerjakan tugas1

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan PHP</title>
+    <style>
+        h1 { color : blue
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo "Hello World!"; ?></h1>
+    <p><?php echo "Nama saya Lestari Vebrianti"; ?></p>
+</body>
+</html>
---
+++ tugas1/1a.php
@@ -0,0 +1,6 @@
+<?php
+$topi = "Topi";
+$bundar = "Bundar";
+
+echo "<h2>\"<i>" . $topi . " saya " . $bundar . ", " . $bundar . " " . $topi . " saya.</i>\"</h1>";
+?>
